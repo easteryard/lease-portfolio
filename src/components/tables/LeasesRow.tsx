fe7575b9465b5a8eb71b8e478d8e9ed486b97ae1
Casper Østergaard <casper@acto.dk>
2021-02-27 20:43:03 +0100

    Added leasesRow

--- src/components/tables/LeasesRow.tsx
+++ src/components/tables/LeasesRow.tsx
@@ -1,21 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { makeStyles } from '@material-ui/core/styles'
+import { Button, Collapse, IconButton, TableCell, TableRow } from '@material-ui/core'
+import { ILease } from '../provider/PortfolioProvider'
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown'
+import KeyboardArrowUpIcon from '@material-ui/icons/KeyboardArrowUp'
 
 const useStyles = makeStyles(theme => ({
-
+    originalRow: {
+        '& > *': {
+            borderBottom: 'unset'
+        }
+    },
+    collapsibleRow: {
+        paddingTop: 0,
+        paddingBottom: 0
+    }
 }))
 
 interface IProps {
-
+    lease: ILease
+    rowAction: (lease: ILease) => void
+    actionText: string
+    isActionDisabled?: (lease: ILease) => boolean
 }
 
-export default function LeasesRow ({  }: IProps) {
+export default function LeasesRow ({ lease, rowAction, actionText, isActionDisabled }: IProps) {
     const classes = useStyles()
+    const [isOpen, setIsOpen] = useState(false)
 
     return (
-        <>
-            
-        </>
+        <React.Fragment>
+            <TableRow className={classes.originalRow}>
+                <TableCell>
+                    <IconButton size="small" onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
+                    </IconButton>
+                </TableCell>
+                <TableCell>{lease.streetName}</TableCell>
+                <TableCell>{lease.houseNumber}</TableCell>
+                <TableCell>{lease.postNumber}</TableCell>
+                <TableCell>{lease.postNumberName}</TableCell>
+                <TableCell align='right' colSpan={7}>
+                    <Button onClick={() => rowAction(lease)}
+                            disabled={isActionDisabled ? isActionDisabled(lease) : false}
+                            variant='outlined' color='primary'
+                    >
+                        {actionText}
+                    </Button>
+                </TableCell>
+            </TableRow>
+            <TableRow>
+                <TableCell colSpan={6} className={classes.collapsibleRow}>
+                    <Collapse in={isOpen} unmountOnExit>
+                        <div>
+                            <p>Hello</p>
+                        </div>
+                    </Collapse>
+                </TableCell>
+            </TableRow>
+        </React.Fragment>
     )
 }
--- src/components/tables/LeasesTable.tsx
+++ src/components/tables/LeasesTable.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import TableTemplate from './TableTemplate'
 import { Button, Collapse, TableBody, TableCell, TableRow } from '@material-ui/core'
 import { ILease } from '../provider/PortfolioProvider'
+import LeasesRow from './LeasesRow'
 
 interface IProps {
     data: ILease[]
@@ -17,32 +18,35 @@ interface IProps {
 export default function LeasesTable ({ data, rowAction, actionText, isActionDisabled, page, handleChangePage,
                                      isExpandable }: IProps) {
     const columns = [
+        '',
         'Vejnavn',
         'Husnr.',
         'Postnr.',
         'Postnummernavn',
         ''
     ]
-
+console.log('data: ', data)
     const tableBody = (
         <TableBody>
             {data.map(lease => (
-                <React.Fragment key={lease.id}>
-                    <TableRow>
-                        <TableCell>{lease.streetName}</TableCell>
-                        <TableCell>{lease.houseNumber}</TableCell>
-                        <TableCell>{lease.postNumber}</TableCell>
-                        <TableCell>{lease.postNumberName}</TableCell>
-                        <TableCell align='right' colSpan={7}>
-                            <Button onClick={() => rowAction(lease)}
-                                    disabled={isActionDisabled ? isActionDisabled(lease) : false}
-                                    variant='outlined' color='primary'
-                            >
-                                {actionText}
-                            </Button>
-                        </TableCell>
-                    </TableRow>
-                </React.Fragment>
+                <LeasesRow key={lease.id} lease={lease} rowAction={rowAction}
+                           actionText={actionText} isActionDisabled={isActionDisabled} />
+                // <React.Fragment key={lease.id}>
+                //     <TableRow>
+                //         <TableCell>{lease.streetName}</TableCell>
+                //         <TableCell>{lease.houseNumber}</TableCell>
+                //         <TableCell>{lease.postNumber}</TableCell>
+                //         <TableCell>{lease.postNumberName}</TableCell>
+                //         <TableCell align='right' colSpan={7}>
+                //             <Button onClick={() => rowAction(lease)}
+                //                     disabled={isActionDisabled ? isActionDisabled(lease) : false}
+                //                     variant='outlined' color='primary'
+                //             >
+                //                 {actionText}
+                //             </Button>
+                //         </TableCell>
+                //     </TableRow>
+                // </React.Fragment>
             ))}
         </TableBody>
     )
